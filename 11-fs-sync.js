18ba4fc00ce4da6cf72b31d0a6f3d508c29a746f
tundeph <54335828+tundeph@users.noreply.github.com>
2023-02-05 21:39:42 +0000

    Update 11-fs-sync.js

--- 11-fs-sync.js
+++ 11-fs-sync.js
@@ -1,25 +1,25 @@
 //-------------------
 
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs")
 
 const test = readFile("./content/subfolder/test.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+	if (err) {
+		console.log(err)
+		return
+	}
 
-  const write = result;
+	const write = result
 
-  writeFile(
-    "./content/subfolder/write.txt",
-    ` This is the content of test.txt file: ${result}`,
-    { flag: "a" },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(write);
-    }
-  );
-});
+	writeFile(
+		"./content/subfolder/write.txt",
+		` This is the content of test.txt file: ${result}`,
+		{ flag: "a" },
+		(err, result) => {
+			if (err) {
+				console.log(err)
+				return
+			}
+			console.log(write)
+		}
+	)
+})
